8dd24066724bfbf7d149905ad164994d685b876e
BytesHouse <a.lihtgolt@gmail.com>
2024-08-21 15:31:21 +0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
+import style from './style.module.css'
 
 function App() {
+  const handleSubmit = () => {
+    fetch('http://127.0.0.1:5500');
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form className={style.form} onSubmit={handleSubmit} action="#">
+        <input type="text" placeholder='Login' />
+        <input type="password" placeholder='Password' />
+        <input type="submit" value="Registration" />
+      </form>
     </div>
   );
 }
